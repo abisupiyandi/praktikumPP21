caf5966dc0239141ff60c9836036632cb7e4e68d
abisupiyandi <89494213+abisupiyandi@users.noreply.github.com>
2021-08-26 21:18:59 +0700

    Update 2-import.py

--- 2-import.py
+++ 2-import.py
@@ -2,6 +2,8 @@
 #Buat file dengan header, misalkan terdiri dari Nama Pasien, Umur, Penyakit, Gender seperti pada tabel berikut dan simpan kedalam format CSV dan Text (Tab delimeter)
 #data disimpan dalam folder data/
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Membaca data dari file dengan format CSV
 data = pd.read_csv("./data/data.csv", sep=";")
@@ -12,6 +14,27 @@ print("\n read text data with tab delimiter")
 with open ('./data/data.txt') as data:
 print(data.read())  
 
-import pandas as pd
 f = pd.read_csv('http://www.exploredata.net/ftp/Spellman.csv')
 print(f)
+
+
+#Membaca file dan menyajikan dalam bentuk grafik. 
+traffic = sp.genfromtxt("web_traffic.tsv",delimiter='\t')
+print(traffic[:10])
+print(traffic.shape)
+
+x = traffic[:,0]
+y = traffic[:,1]
+
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
+
+plt.scatter(x,y)
+plt.title("Web traffic last month")
+plt.xlabel("Time")
+plt.ylabel("Hits/hour")
+
+plt.xticks([w*7*24 for w in range(10)],['week %i' %w for w in range(10)])
+plt.autoscale(tight=True)
+plt.grid()
+plt.show()
